50b01a3a4ade25e81214178156c79b98804b8844
Mette Roen <metteway@gmail.com>
2026-04-23 08:46:42 +0200

    fhhkjfdhjk

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ app.get('/', (req, res) => {
                 <div class="practical-info">
                     <div class="info-item">
                         <h4>💳 Betaling</h4>
-                        <p>Betaling sker via MobilePay eller kontant ved afhentning på gården. OBS: Bemærk priserne er vejledende ift. forventede mængder.</p>
+                        <p>Betaling sker via MobilePay eller kontant ved afhentning på gården. Den endelige pris udregnes ved afhentning (Afhængigt af kg/udskæring).</p>
                     </div>
                     <div class="info-item">
                         <h4>🚜 Afhentning</h4>
